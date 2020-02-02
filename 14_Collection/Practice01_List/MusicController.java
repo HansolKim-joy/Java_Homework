bda4ae8be928bca56f7c23178b76f8f9fd366319
HansolKim-joy <60562021+HansolKim-joy@users.noreply.github.com>
2020-02-02 18:44:45 +0900

    Add files via upload

---
+++ 14_Collection/Practice01_List/MusicController.java
@@ -0,0 +1,71 @@
+package com.kh.practice.list.music.controller;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.kh.practice.list.music.model.compare.AscTitle;
+import com.kh.practice.list.music.model.vo.Music;
+
+public class MusicController {
+	List<Music> list = new ArrayList<Music>();
+	
+	public int addList(Music music) {
+		list.add(music);
+		return 1;
+	}
+	
+	public int addAtZero(Music music) {
+		list.add(0, music);
+		return 1;
+	}
+	
+	public List printAll() {
+		return list;
+	}
+	
+	public Music searchMusic(String title) {
+		Music m = null;
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i).getTitle().equals(title)) {
+				m = list.get(i);
+				break;
+			}
+		}
+		return m;
+	}
+	
+	public Music removeMusic(String title) {
+		Music m = null;
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i).getTitle().equals(title)) {
+				m = list.get(i);
+				list.remove(i);
+				break;
+			}
+		}
+		return m;
+	}
+	
+	public Music setMusic(String title, Music music) {
+		Music m = null;
+		for(int i = 0; i < list.size(); i++) {
+			if(list.get(i).getTitle().equals(title)) {
+				m = list.remove(i);
+				list.add(music);
+				break;
+			}
+		}
+		return m;
+	}
+	
+	public int ascTitle() {
+		Collections.sort(list);
+		return 1;
+	}
+	
+	public int descSinger() {
+		list.sort(new AscTitle());
+		return 1;
+	}
+}
