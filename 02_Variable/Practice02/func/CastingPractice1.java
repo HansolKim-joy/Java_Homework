620e679cbb9f99d18de72b4895ccce1da7410fd1
HansolKim-joy <60562021+HansolKim-joy@users.noreply.github.com>
2020-02-02 17:03:00 +0900

    Rename 02_Variable/CastingPractice1.java to 02_Variable/Practice02/func/CastingPractice1.java

--- 02_Variable/Practice02/func/CastingPractice1.java
+++ 02_Variable/Practice02/func/CastingPractice1.java
@@ -6,11 +6,11 @@ public class CastingPractice1 {
 	public void practice1() {
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.print("¹®ÀÚ : ");
+		System.out.print("Â¹Â®Ã€Ãš : ");
 		char ch1 = sc.nextLine().charAt(0);
 		System.out.println(ch1 + " unicode : " + (int)ch1);
 		
-		System.out.print("¹®ÀÚ : ");
+		System.out.print("Â¹Â®Ã€Ãš : ");
 		char ch2 = sc.nextLine().charAt(0);
 		System.out.println(ch2 + " unicode : " + (int)ch2);
 		
